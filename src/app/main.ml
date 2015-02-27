55ac28a0ada5238655adbfec486a48b8aacea888
Sebastien Mondet <seb@mondet.org>
2015-02-27 17:18:08 -0500

    Improve tests / and error management

--- src/app/main.ml
+++ src/app/main.ml
@@ -21,20 +21,24 @@ let write_lines ~path l =
 let generate_test_fasta ~path =
   write_lines ~path [
     ">1 bla bla";
-    "NNNNNNNNNN";
-    "NNNNNNNNNN";
-    "NNNNNNNNNN";
-    "ACGTCNTNTC";
-    "CAGCNTNNTC";
-    "CCACGCCTCC";
-    "CCACGCCTCC";
-    "CCAGCCCCTC";
+    "NNNNN";
+    "NNNNN";
+    "NNNNN";
+    "ACGTC";
+    "CAGCN";
+    "CCACG";
+    "CCACG";
+    "CCAGC";
     ">2 bla bla";
     ">3 bla bla";
-    "ACGTCNTNTC";
-    "CAGCNTNNTC";
-    "CCACGCCTCC";
-    "NNNNNNNNNN";
+    "ACGTC";
+    "NTNTC";
+    "CAGCN";
+    "TNNTC";
+    "CCACG";
+    "CCTCC";
+    "NNNNN";
+    "NNNNN";
   ]
 
 let generate_test_dbnsp ~path =
@@ -62,92 +66,102 @@ let generate_test_dbnsp ~path =
     variant 3 6 "NTN" "N";
   ]
 
-let test_load files =
+let test_load ~verbose ~fasta ~dbsnp =
   let outline fmt = ksprintf (fun s -> printf "%s\n" s; return ()) fmt in
   let print_stats msg =
-    printf "\n=== %s %s ===\nGC stats:\n" msg Plawireg.Time.(now () |> to_filename);
-    Gc.print_stat stdout;
-    IO.read_file "/proc/self/status"
-    >>= fun status ->
-    printf "\n/proc/self/status:\n%s\n%!" status;
-    return () in
-  Deferred_list.while_sequential files ~f:(fun f ->
-      Plawireg.Graph.create ()
-      >>= fun graph ->
-      print_stats "before load_reference" >>= fun () ->
-      Plawireg.Graph.load_reference graph f
-      >>= fun () ->
+    if verbose then
+      begin
+        printf "\n=== %s %s ===\nGC stats:\n" msg Plawireg.Time.(now () |> to_filename);
+        Gc.print_stat stdout;
+        IO.read_file "/proc/self/status"
+        >>= fun status ->
+        printf "\n/proc/self/status:\n%s\n%!" status;
+        return ()
+      end
+    else
+      begin
+        printf "\n=== %s %s ===\n" msg Plawireg.Time.(now () |> to_filename);
+        return ()
+      end
+  in
+  Plawireg.Graph.create ()
+  >>= fun graph ->
+  print_stats "before load_reference" >>= fun () ->
+  Plawireg.Graph.load_reference graph ~path:fasta
+  >>= fun () ->
       (*
       let out = open_out "./datadumb.bin" in
       Marshal.to_channel out graph [Marshal.No_sharing];
       close_out out;
       *)
-      printf "Roots:\n   %s\n%!"
-        (Plawireg.Graph.root_names graph |> String.concat ~sep:"\n   ");
-      Plawireg.Graph.count_nodes graph
-      >>= fun counts ->
+  printf "Roots:\n   %s\n%!"
+    (Plawireg.Graph.chromosome_names graph |> String.concat ~sep:"\n   ");
+  Plawireg.Graph.add_vcf graph ~path:dbsnp
+  >>= fun () ->
+  Plawireg.Graph.count_nodes graph
+  >>= fun counts ->
+  let () =
+    printf "\nCounts:\n";
+    List.iter  counts (fun (name, count) ->
+        printf "* %s: %d nodes\n" name.Plawireg.Graph.chromosome count
+      ) in
+  print_stats "before fold" >>= fun () ->
+  Plawireg.Graph.fold graph ~init:() ~f:(fun () -> function
+    | `Name n -> outline "» %s" n.Plawireg.Graph.chromosome
+    | `Node node ->
+      (* outline "  %s → %s" *)
+      (*   (Plawireg.Unique_id.to_string id) (Plawireg.Sequence.to_string seq) *)
+      return ()
+    )
+  >>= fun () ->
+  print_stats "before second fold" >>= fun () ->
+  Plawireg.Graph.fold graph ~init:([]) ~f:(fun prev -> function
+    | `Name n -> return (n.Plawireg.Graph.chromosome :: prev)
+    | `Node node ->
+      Plawireg.Graph.expand_node graph node
+      >>= fun (id, kind, seq) ->
+      return (sprintf "%s → %s"
+                (Plawireg.Unique_id.to_string id)
+                (Plawireg.Sequence.to_string seq)
+              :: prev)
+      (* outline "  %s → %s" *)
+      (*   (Plawireg.Unique_id.to_string id)  *)
+    )
+  >>= fun list_of_strings ->
+  let () =
+    printf "Names and Seqs:\n";
+    begin try
+      List.iteri (List.rev list_of_strings) ~f:(fun i x ->
+          if i > 100 then raise Not_found
+          else
+            printf "  %s\n" x)
+    with _ -> (printf "  ...\n")
+    end
+  in
+  let base_counts = Hashtbl.create 42 in
+  Plawireg.Graph.fold graph ~init:"" ~f:(fun name -> function
+    | `Name n ->
+      Hashtbl.add base_counts n.Plawireg.Graph.chromosome (Hashtbl.create 42);
+      return n.Plawireg.Graph.chromosome
+    | `Node node ->
+      Plawireg.Graph.expand_node graph node
+      >>= fun (id, kind, seq) ->
       let () =
-        printf "\nCounts:\n";
-        List.iter  counts (fun (name, count) ->
-            printf "* %s: %d nodes\n" name count
-          ) in
-      print_stats "before fold" >>= fun () ->
-      Plawireg.Graph.fold graph ~init:() ~f:(fun () -> function
-        | `Name n -> outline "» %s" n
-        | `Node node ->
-          (* outline "  %s → %s" *)
-          (*   (Plawireg.Unique_id.to_string id) (Plawireg.Sequence.to_string seq) *)
-          return ()
-        )
-      >>= fun () ->
-      print_stats "before second fold" >>= fun () ->
-      Plawireg.Graph.fold graph ~init:([]) ~f:(fun prev -> function
-        | `Name n -> return (n :: prev)
-        | `Node node ->
-          Plawireg.Graph.expand_node graph node
-          >>= fun (id, kind, seq) ->
-          return (sprintf "%s → %s"
-                    (Plawireg.Unique_id.to_string id)
-                    (Plawireg.Sequence.to_string seq)
-                  :: prev)
-          (* outline "  %s → %s" *)
-          (*   (Plawireg.Unique_id.to_string id)  *)
-        )
-      >>= fun list_of_strings ->
-      let () =
-        printf "Names and Seqs:\n";
-        begin try
-          List.iteri (List.rev list_of_strings) ~f:(fun i x ->
-              if i > 100 then raise Not_found
-              else
-                printf "  %s\n" x)
-        with _ -> (printf "  ...\n")
-        end
+        String.iter (Plawireg.Sequence.to_string seq) ~f:(fun c ->
+            let ht = Hashtbl.find base_counts name in
+            match Hashtbl.find ht c with
+            | v -> incr v
+            | exception e -> Hashtbl.add ht c (ref 1))
       in
-      let base_counts = Hashtbl.create 42 in
-      Plawireg.Graph.fold graph ~init:"" ~f:(fun name -> function
-        | `Name n -> Hashtbl.add base_counts n (Hashtbl.create 42); return n
-        | `Node node ->
-          Plawireg.Graph.expand_node graph node
-          >>= fun (id, kind, seq) ->
-          let () =
-            String.iter (Plawireg.Sequence.to_string seq) ~f:(fun c ->
-                let ht = Hashtbl.find base_counts name in
-                match Hashtbl.find ht c with
-                | v -> incr v
-                | exception e -> Hashtbl.add ht c (ref 1))
-          in
-          return name)
-      >>= fun (_ : string) ->
-      printf "\nSome Stats:\n%!";
-      let disp chr ht =
-        printf "- %s:\n" chr;
-        Hashtbl.iter (fun c cr -> printf "  %c\t%d\n" c !cr) ht
-      in
-      Hashtbl.iter disp base_counts;
-      print_stats "last one" >>= fun () ->
-      return ())
-  >>= fun (_ : unit list) ->
+      return name)
+  >>= fun (_ : string) ->
+  printf "\nSome Stats:\n%!";
+  let disp chr ht =
+    printf "- %s:\n" chr;
+    Hashtbl.iter (fun c cr -> printf "  %c\t%d\n" c !cr) ht
+  in
+  Hashtbl.iter disp base_counts;
+  print_stats "last one" >>= fun () ->
   return ()
 
 let () =
@@ -159,8 +173,8 @@ let () =
       | "dbsnp" -> generate_test_dbnsp ~path
       | other -> failwithf "Unknown file-kind to generate"
       end
-    | "test-load" :: files ->
-      test_load files
+    | "test-load" :: fasta :: dbsnp :: [] ->
+      test_load ~verbose:false ~fasta ~dbsnp
     | "test-uid" :: _ ->
       printf "Test-UID:\n%!";
       printf "- time: %f s\n%!" (Plawireg.Unique_id.test ());
--- src/lib/plawireg.ml
+++ src/lib/plawireg.ml
@@ -73,6 +73,7 @@ module Pointer = struct
   type id = Unique_id.t
   type 'a t = {id : id}
   let create id = {id}
+  let to_string {id} = Unique_id.to_string id
 end
 module Cache = struct
   type 'a t = (Pointer.id, 'a) Hashtbl.t
@@ -97,10 +98,12 @@ module Sequence: sig
   type t
   val of_string_exn: string -> t
   val to_string: t -> string
+  val length: t -> int
 end = struct
   type t = string
   let of_string_exn s = s
   let to_string a = a
+  let length = String.length
 end
 module Node = struct
   type t = {
@@ -111,20 +114,33 @@ module Node = struct
     parent: t Pointer.t option;
   }
   let pointer t = Pointer.create t.id
+  let to_string { id; kind; sequence; next; parent } =
+    sprintf "{%s: %s (%s, %s)}"
+      (Unique_id.to_string id)
+      (match kind with
+       | `Db_snp s -> sprintf "DB:%s" s
+       | `Reference -> sprintf "Ref"
+       | `Cosmic s -> sprintf "DB:%s" s)
+      (match Array.to_list next with
+       | [] -> "no-next"
+       | a ->  "next:" ^ String.concat ~sep:"," (List.map ~f:Pointer.to_string a))
+      (Option.value_map parent ~default:"root"
+         ~f:(fun p -> sprintf "parent:%s" (Pointer.to_string p)))
 end
 module Graph = struct
+  type root_key = {chromosome: string; comment: string}
   type t = {
     sequences: Sequence.t Cache.t;
     nodes: Node.t Cache.t;
-    mutable roots: (string * Node.t Pointer.t option) list;
+    mutable roots: (root_key * Node.t Pointer.t option) list;
   }
   let create () =
     let nodes = Cache.create () in
     let sequences = Cache.create () in
     return {sequences; nodes; roots = []}
 
-  let root_names t =
-    List.map t.roots ~f:fst
+  let chromosome_names t =
+    List.map t.roots ~f:(fun ({chromosome; _}, _) -> chromosome)
 
   let roots t = t.roots
 
@@ -196,26 +212,27 @@ module Graph = struct
 
   let fold_lines path ~on_exn ~init ~f : (_, _) Deferred_result.t  =
     let stream = Lwt_io.lines_of_file path in
-    let rec loop prev = 
-      wrap_deferred ~on_exn (fun () -> Lwt_stream.get stream)
+    let rec loop line_number prev = 
+      wrap_deferred ~on_exn:(on_exn ~line_number)
+        (fun () -> Lwt_stream.get stream)
       >>= begin function
       | Some s ->
-        f prev s
+        (try f ~line_number prev s with e -> fail (on_exn ~line_number e))
         >>= fun next ->
-        loop next
+        loop (line_number + 1) next
       | None -> return prev
       end
     in
-    loop init
+    loop 1 init
 
   let load_reference: t -> path:string -> (unit, _) Deferred_result.t =
     fun t ~path ->
       (* let next_id = ref (Unique_id.create ()) in *)
       (* let current_root = ref None in *)
-      let node_of_line ?parent line =
+      let node_of_line ~line_number ?parent line =
         begin match Sequence.of_string_exn line with
         | s -> return s
-        | exception e -> fail (`Graph (`Load_fasta (path, e)))
+        | exception e -> fail (`Graph (`Load_fasta (path, line_number, e)))
         end
         >>= fun sequence ->
         let sequence_id = Unique_id.create () in
@@ -228,25 +245,36 @@ module Graph = struct
                     next = [|  |];
                     parent;} in
         return node in
-      fold_lines path ~init:(`None) ~f:(fun prev line ->
+      let root_key_of_line line =
+        match
+          line |> String.split ~on:(`Character ' ')
+          |> List.filter_map
+            ~f:(fun s -> match String.strip s with "" -> None | st -> Some st)
+        with
+        | chromosome :: more :: than_one ->
+          {chromosome; comment = String.concat ~sep:" " (more :: than_one)}
+        | other -> failwithf "Cannot find chromosome name: %S" line
+      in
+      fold_lines path ~init:(`None) ~f:(fun ~line_number prev line ->
           (* dbg "line: %S" line; *)
           match prev, line with
           | `None, line when String.get line 0 = Some '>' ->
             let root_name =
               String.(sub_exn line ~index:1 ~length:(length line - 1)) in
             return (`New_name root_name)
-          | `None, l -> fail (`Graph (`Wrong_fasta (`First_line l)))
+          | `None, l ->
+            fail (`Graph (`Wrong_fasta (`First_line (path, line_number, l))))
           | `New_name n, line when String.get line 0 = Some '>' ->
             (* empty fasta sequence *)
-            t.roots  <- t.roots @ [(n, None)];
+            t.roots  <- t.roots @ [(root_key_of_line n, None)];
             let root_name =
               String.(sub_exn line ~index:1 ~length:(length line - 1)) in
             return (`New_name root_name)
           | `New_name n, line ->
             (* first node of the new root *)
-            node_of_line ?parent:None line
+            node_of_line ~line_number ?parent:None line
             >>= fun node ->
-            t.roots <- t.roots @ [n, Some (Node.pointer node)];
+            t.roots <- t.roots @ [root_key_of_line n, Some (Node.pointer node)];
             return (`Node node)
           | `Node node, line when String.get line 0 = Some '>' ->
             Cache.store t.nodes ~at:node.Node.id ~value:node
@@ -255,7 +283,7 @@ module Graph = struct
               String.(sub_exn line ~index:1 ~length:(length line - 1)) in
             return (`New_name root_name)
           | `Node node, line ->
-            node_of_line ~parent:(Node.pointer node) line
+            node_of_line ~line_number ~parent:(Node.pointer node) line
             >>= fun (new_node) ->
             let to_store =
               {node with Node.next = [| Node.pointer new_node |] } in
@@ -263,23 +291,27 @@ module Graph = struct
             >>= fun () ->
             return (`Node new_node)
         )
-        ~on_exn:(fun e -> `Graph (`Load_fasta (path, e)))
+        ~on_exn:(fun ~line_number e ->
+            `Graph (`Load_fasta (path, line_number, e)))
       >>= function
       | `New_name name ->
-        t.roots  <- t.roots @ [(name, None)]; return ()
+        t.roots  <- t.roots @ [(root_key_of_line name, None)]; return ()
       | `Node node ->
         Cache.store t.nodes ~at:node.Node.id ~value:node
       | `None -> return ()
 
-  let add_vcf: t -> path:string -> unit =
+  let add_vcf: t -> path:string -> (unit, _) Deferred_result.t =
     fun t ~path ->
-      assert false
+      dbg "Ignoring %S" path;
+      return ()
 
   module Error = struct
     let to_string = function
-    | `Load_fasta (file, e) ->
-      sprintf "Load_fasta: %S: %s" file (Exn.to_string e)
-    | `Wrong_fasta (`First_line l) ->
-      sprintf "Wrong_fasta: first-line %S" l
+    | `Load_fasta (file, line, e) ->
+      sprintf "Load_fasta: %S:%d %s" file line (Exn.to_string e)
+    | `Load_vcf (file, line, e) ->
+      sprintf "Load_vcf: %S:%d %s" file line (Exn.to_string e)
+    | `Wrong_fasta (`First_line (path, line, l)) ->
+      sprintf "Wrong_fasta: %S:%d: first-line not '>' %S" path line l
   end
 end
